1d340093d28b1a25c2e6dd24577cc95bb184620d
subba <subba@Subba-lenovo.lan>
2023-03-13 21:17:00 -0400

    tesy

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 @ComponentScan
-public class DemoApplication extends SpringBootServletInitializer{
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
