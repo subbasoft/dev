dc0e0d9435b73e0587d731a3a58834cb35cecc1c
subba <subba@Subba-lenovo.lan>
2023-03-13 21:05:20 -0400

    cross

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @ComponentScan
 public class DemoApplication {
 
