83fcb883ca2c470e797fc001cb61f769cafebe9e
subba <subba@Subba-lenovo.lan>
2023-03-13 22:40:17 -0400

    rr

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@CrossOrigin
-@ComponentScan
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
