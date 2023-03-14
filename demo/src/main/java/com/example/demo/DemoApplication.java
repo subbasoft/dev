f7c6b67849ba138854e2d12335d3ca357ec7cc05
subba <subba@Subba-lenovo.lan>
2023-03-13 20:45:04 -0400

    application

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
